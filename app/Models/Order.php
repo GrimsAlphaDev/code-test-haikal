95eabd7beabe6e597b2a65a75d17af829f2f2e41
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-11-12 12:42:23 +0700

    update database migration

---
+++ app/Models/Order.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'customer_id',
+        'menu_id',
+        'quantity',
+        'total',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
---
+++ database/migrations/2024_11_12_050202_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->onDelete('cascade');
+            $table->foreignId('status_id')->constrained('order_statuses')->onDelete('cascade');
+            $table->date('delivery_date');
+            $table->string('payment_proof')->nullable();
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
